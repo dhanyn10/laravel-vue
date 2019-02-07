5e4be23900b071b6e74c8e5171b93239969b0de7
dhanyn10 <dhanyn.dhan@gmail.com>
2019-02-07 15:37:56 +0700

    update

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+@section('content')
+<div id="app">
+    <div class="container">
+        <Articles></Articles>
+    </div>
+</div>
+<script src="{{asset('js/app.js')}}"></script>
+@endsection
